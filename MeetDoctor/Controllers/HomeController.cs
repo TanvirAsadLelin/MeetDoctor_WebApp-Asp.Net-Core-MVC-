80cc7c2e6060f2feaa39becdeb1ce69de59b95ad
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-13 12:27:10 +0600

    MeetDoctor web project first startup from my learn 1st commit

---
+++ MeetDoctor/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetDoctor.Controllers
+{    
+     [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
